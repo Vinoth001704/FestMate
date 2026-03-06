2e87f0e94542da96847d15f46ad8a713ff896f6a
Vinoth001704 <vinothkumar00023@gmail.com>
2026-03-06 15:39:03 +0530

    security update

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ dotenv.config()
 
 const app=express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://fest-mate-web-app.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true
+}));
 let port=process.env.PORT;
 
 import userRoutes from "./routes/user.js";
